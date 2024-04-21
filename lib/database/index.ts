7722aa11b5e8ce43a44be337123c5b270bc57d27
Jeffreyjacob <jeffreyigiemeh16@gmail.com>
2024-04-21 09:15:13 -0700

    new commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,19 +1,32 @@
-import monogoose from 'mongoose';
+import monogoose, { Mongoose } from 'mongoose';
 
 const MONOGODB_URI = process.env.MONOGODB_URI;
 
-let cached = monogoose || {connect:null,Promise:null};
+interface MongooseConn{
+    conn:Mongoose | null,
+    promise:Promise<Mongoose> | null
+}
+
+let cached:MongooseConn = (global as any).mongoose; 
+if(!cached){
+    cached = (global as any).mongoose = {
+        conn:null,
+        promise:null
+    }
+}
 
 export const connectToDatabase = async ()=>{
-    if(cached.connect) return cached.connect;
+    if (cached.conn) return cached.conn;
+   
     if(!MONOGODB_URI) throw new Error('MONOGODB_URI is missing');
 
-    cached.Promise = cached.Promise || monogoose.connect(MONOGODB_URI,{
+    cached.promise = cached.promise || monogoose.connect(MONOGODB_URI,{
         dbName:'evently',
-        bufferCommands:false
+        bufferCommands:false,
+        connectTimeoutMS:30000
     })
 
-    cached.connect = await cached.Promise;
+    cached.conn = await cached.promise;
 
-    return cached.connect;
+    return cached.conn;
 }
