77f7e5df7384232154189d5bcb3ba1acf2799372
Jeffreyjacob <jeffreyigiemeh16@gmail.com>
2024-04-24 04:14:59 -0700

    new commit

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,18 +1,28 @@
 import { Button } from '@/components/ui/button'
 import Collection from '@/components/ui/shared/Collection'
 import { getEventsByUser } from '@/lib/actions/event.action'
+import { getOrdersByUser } from '@/lib/actions/order.action'
+import { IOrder } from '@/lib/database/Modals/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import { Organization } from '@clerk/nextjs/server'
 import Link from 'next/link'
 import React from 'react'
 
-const page = async () => {
+const page = async ({searchParams}:SearchParamProps) => {
     const {sessionClaims} = auth();
     const userId = sessionClaims?.userId as string;
 
+    const ordersPage =  Number(searchParams?.ordersPage) || 1;
+    const eventsPage =  Number(searchParams?.eventsPage) || 1;
+
     const organizedEvents = await getEventsByUser({
-     userId,page:1
+     userId,page:eventsPage
     })
+    const orders = await getOrdersByUser({
+        userId,page:ordersPage
+    })
+    const orderedEvents = orders?.data.map((order:IOrder)=> order.event) || [];
   return (
     <>
     {/**My Ticket */}
@@ -27,16 +37,16 @@ const page = async () => {
      </div>
     </section>
 
-    {/* <section className='wrapper my-8'>
-    <Collection data={events?.data} 
+    <section className='wrapper my-8'>
+    <Collection data={orderedEvents} 
       emptyTitle = 'No Events ticket purchased yet'
       emptyStateSubText = "No worries - plenty of exciting events to explore!"
       collectionType="My_Tickets"
       limit={3}
-      page={1}
-      totalPages={2}
+      page={ordersPage}
+      totalPages={orders?.totalPages}
       urlParamName='ordersPage'/>
-    </section> */}
+    </section> 
 
     {/**Event Organized */}
     <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
@@ -56,8 +66,8 @@ const page = async () => {
       emptyStateSubText = "Go create some now!"
       collectionType="Events_Organized"
       limit={6}
-      page={1}
-      totalPages={2}
+      page={eventsPage}
+      totalPages={organizedEvents?.totalPages}
       urlParamName='eventsPage'/>
     </section>
     </>
